c3ae46b0f3ef7fdf9fc08824750146ddea25e261
Talles Pellense <160547176+tallesSENAI@users.noreply.github.com>
2024-04-16 21:38:49 -0300

    Add files via upload

---
+++ arquivoJS.js
@@ -0,0 +1,6 @@
+/*Enviando mensagens para o console do site apenas para teste*/
+console.log("outro teste");
+console.log(1 + 1);
+
+document.getElementById('nome').style.backgroundColor="Green"; /*como foi criado um ID 'nome' para o INPUT DE TEXTOS,
+é possível buscá-lo pelo ID e alterar algumas características caso for preciso*/
